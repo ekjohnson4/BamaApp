11278c1fa6c89c28eae8ce8d68a779deca79a5d9
tgillam <tgillam12@gmail.com>
2018-11-13 12:46:09 -0600

    Update events.ts

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { Article1Page } from '../news/articles/article1';
+import { News } from '../news/news';
+import { Headline1Page } from '../home/headlines/headline1/headline1';
+import { Headline2Page } from '../home/headlines/headline2/headline2';
+import { Headline3Page } from '../home/headlines/headline3/headline3';
+
 
 @IonicPage()
 @Component({
@@ -21,6 +27,29 @@ export class Events {
   }
 
   btnClicked(){
-   alert("not implemented");
+   this.navCtrl.setRoot(HomePage);
   }
+
+  onInput(event){
+
+  }
+
+  openArticle(article) {
+    console.log('Opening article ' + article);
+    if(article == 1) {
+      this.navCtrl.push(Article1Page);
+    }
+    else if(article == 2){
+      this.navCtrl.push(News);
+    }
+    else if(article == 3){
+      this.navCtrl.push(Headline1Page);
+    }
+    else if(article == 4){
+      this.navCtrl.push(Headline2Page);
+    }
+    else if(article == 5){
+      this.navCtrl.push(Headline3Page);
+    }
+  }  
 }
