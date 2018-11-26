fb4b1c1b242d69ffd5c2fa9591294fb07b8e7eb9
Jeremy <jjmccormick@crimson.ua.edu>
2018-11-26 17:38:07 -0600

    Filled out view person data

--- src/pages/campus-directory/view-person.ts
+++ src/pages/campus-directory/view-person.ts
@@ -10,9 +10,13 @@ export class ViewPerson {
   @ViewChild(Content) content: Content;
   name: string = '';
   email: string = '';
+  phone: string = '';
+  office: string = '';
+  department: string = '';
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public loadingCtrl: LoadingController, public eventMenu: Events) {
     this.name = navParams.get('name');
+    this.makeData()
   }
 
   goToPage(page) {
@@ -40,5 +44,17 @@ export class ViewPerson {
     this.navCtrl.pop();
   }
 
-  
+  makeData() {
+    if(this.name == null || this.name == '')
+      return;
+    //Get first letter and last name to get email
+    let firstLetter = this.name[0].toLowerCase();
+    let lastName = this.name.slice(this.name.indexOf(" "),this.name.length).toLowerCase();
+    let emailName = firstLetter.concat(lastName);
+    this.email= emailName.concat('@crimson.ua.edu');
+    //Set phone number
+    this.phone = '(555)-555-5555';
+    this.office = '100 Lloyd Hall';
+    this.department = 'Athletics';
+  }
 }
