165f9802a37d69bb6e128ca4e4a5d4bda60c6706
tgillam <tgillam12@gmail.com>
2018-11-28 23:09:12 -0600

    Added notification when done

--- src/pages/laundry/burke.ts
+++ src/pages/laundry/burke.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController, Content } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, Content, AlertController } from 'ionic-angular';
 import { HomePage } from '../home/home';
 
 @IonicPage()
@@ -10,7 +10,7 @@ import { HomePage } from '../home/home';
 export class Burke {
   room: string = 'initialName';
   @ViewChild(Content) content: Content;
-  constructor(public navCtrl: NavController, public navParams: NavParams,
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
     public loadingCtrl: LoadingController) {
   }
 
@@ -41,5 +41,39 @@ export class Burke {
 
     console.log('Headline3 back button pressed');
     this.navCtrl.pop();
+  }
+
+  btnClicked(){
+        let alert1 = this.alertCtrl.create({
+          title:'Success!',
+          subTitle:'You will receive a text when the washer/dryer is done running.',
+          buttons:[
+          {
+                    text: 'Done',
+
+        }
+          ]
+        });
+        let alert = this.alertCtrl.create({
+          title:'Notify When Done',
+          subTitle:'Input phone number:',
+                inputs: [ 
+        {
+          name: 'phone',
+          placeholder: 'Phone Number',
+          type: 'tel'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Notify',
+          handler: data => {
+                        alert1.present();
+                    }
+        }
+      ]
+       });
+        alert.present();
+
   }
 }
