266987541ecd23863591ae5dc6a94506e2e0060c
Jeremy <jjmccormick@crimson.ua.edu>
2018-12-01 12:53:15 -0600

    Call phone number when clicked

--- src/pages/emergency/emergency.ts
+++ src/pages/emergency/emergency.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController, Content, Events } from 'ionic-angular';
+import { CallNumber } from '@ionic-native/call-number';
 
 @IonicPage()
 @Component({
@@ -9,7 +10,8 @@ import { IonicPage, NavController, NavParams, LoadingController, Content, Events
 export class Emergency {
   @ViewChild(Content) content: Content;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-  public loadingCtrl: LoadingController, public eventMenu: Events) {
+  public loadingCtrl: LoadingController, public eventMenu: Events,
+  private callNumber: CallNumber) {
   }
 
   goToPage(page) {
@@ -35,5 +37,13 @@ export class Emergency {
 
     console.log('Headline3 back button pressed');
     this.navCtrl.pop();
+  }
+
+  makeCall(phonenum) {
+    if(this.callNumber.isCallSupported()) {
+      this.callNumber.callNumber(phonenum,true)
+        .then(res => console.log("Launched Dialer", res))
+        .catch(err => console.log("Could not launch dialer", err));
+    }
   }
 }
